fd4b37618f33ebe7290a749d907a88be34652b5e
ali <mustofa@extrainteger.com>
2017-11-23 11:59:00 +0700

    add chooser

---
+++ app/src/main/java/com/example/lifecycletest/main/CoolStuffActivity.kt
@@ -0,0 +1,35 @@
+package com.example.lifecycletest.main
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.lifecycletest.R
+import com.example.lifecycletest.lifecycle.MainActivity
+import com.example.lifecycletest.nestedrecyclerview.ListActivity
+import kotlinx.android.synthetic.main.activity_cool_stuff.*
+
+class CoolStuffActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cool_stuff)
+        tvLifecycle.setOnClickListener(this)
+        tvNestedRecycler.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v){
+            tvLifecycle -> openLifecycleActyvity()
+            tvNestedRecycler -> openNestedRecyclerActivity()
+        }
+    }
+
+    private fun openNestedRecyclerActivity() {
+        startActivity(Intent(this@CoolStuffActivity, ListActivity::class.java))
+    }
+
+    private fun openLifecycleActyvity() {
+        startActivity(Intent(this@CoolStuffActivity, MainActivity::class.java))
+    }
+}
